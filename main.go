83ae2440430f804cbbf2d80133f3532633bd9d6e
Christian Uhsat <christian@uhsat.de>
2026-02-28 15:03:24 +0100

    wip

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 // https://github.com/libyal/libevtx/blob/main/documentation/Windows%20XML%20Event%20Log%20(EVTX).asciidoc
 // https://www.researchgate.net/publication/222426407_Introducing_the_Microsoft_Vista_event_log_file_format
+// https://blog.fox-it.com/2019/06/04/export-corrupts-windows-event-log-files/
+// https://ernw.de/download/EventManipulation.pdf
 package main
 
 import (
