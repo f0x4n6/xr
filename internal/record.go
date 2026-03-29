5b0f2bb9c9642a26516c45d5aa9d922c196a9733
Christian Uhsat <christian@uhsat.de>
2026-03-30 01:00:21 +0200

    wip

--- internal/record.go
+++ internal/record.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"io"
@@ -45,6 +46,95 @@ func NewRecord(r io.Reader) *Record {
 		// template instance
 	}
 
+	// Each record starts with a system element
+	/*
+		<Event>
+			<System>
+			</System>
+			<...>
+		</Event>
+
+		Fragment
+			Fragment header
+			0f header token
+			01 major version
+			01 minor version
+			00 flags
+
+			Element or Template instance
+			| Template instance
+				0C template instance token
+				... temp definition
+					[1] unknown
+					[4] unknown
+					[4] temp def data (or offset)
+					[4] unknown (0 = not used)
+					[16] temp id (guid)
+					[4] data size
+					... fragment header
+					... element
+					[1] end of file token (00)
+
+				... temp inst data
+					[4] number of temp values
+					... array temp value desc
+						[2] value size
+						[1] value type
+						00  unknown (empty?)
+
+					... array temp value data
+
+			| Element (empty of filled)
+				| Empty
+					element start
+					close empty element token
+				| Filled
+					01|41 element start (01 = no element, 41 attribute list exists)
+						? 0xffff (optional, dependency identifier)
+						Data size
+						? element name offset
+						Attribute list
+					close start element token
+					content
+					end element token
+
+					Name
+						[4] unknown
+						[2] name hash
+						[2] length (chars)
+						UTF-16 little-endian string with an end-of-string character
+							Idea: parse strings into map?
+
+					Content
+						element / string data / character entity reference / entity reference / CDATA / PI?
+
+						unicode text string
+							[2] length (chars)
+							... UTF-16 little-endian string without an end-of-string character
+
+						string data
+							value test / substitution
+
+							value text
+							05|45 value token
+							01    value type
+							...   value data
+
+							substitution
+								normal / optional
+
+							[4] normal
+								0x0d normal subs token
+								[2] identifier
+								[1] value type
+
+							[4] optional
+								0x0d normal subs token
+								[2] identifier
+								[1] value type
+
+	*/
+
 	return &record
 }
 
@@ -53,13 +143,16 @@ func (r *Record) String() string {
 
 	sb.WriteString(fmt.Sprintf("Id:   %d\n", r.Id))
 	sb.WriteString(fmt.Sprintf("Time: %s\n", r.Time.Format(time.RFC3339)))
-	sb.WriteString(fmt.Sprintf("Size: %d [0x%x]\n", r.Size, r.Size))
-	sb.WriteString(fmt.Sprintf("Copy: %d [0x%x]\n\n", r.Copy, r.Copy))
+	sb.WriteString(fmt.Sprintf("Size: %d [0x%04x]\n", r.Size, r.Size))
+	sb.WriteString(fmt.Sprintf("Copy: %d [0x%04x]\n\n", r.Copy, r.Copy))
 	sb.WriteString(hex.Dump(r.Event))
+
+	eventId := binary.LittleEndian.Uint16(r.Event[0x66:0x68])
+	sb.WriteString(fmt.Sprintf("\nEventID: %d [0x%04x] DEBUG\n", eventId, eventId))
 
 	return sb.String()
 }
 
 func (r *Record) IsValid() bool {
-	return r.Size == r.Copy
+	return r.Size == r.Copy && !r.Time.IsZero()
 }
