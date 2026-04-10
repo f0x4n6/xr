ec54a1b9dbac0ab4cdfbca188af0b6f55fdb0e47
Christian Uhsat <christian@uhsat.de>
2026-04-10 02:01:45 +0200

    wip

--- main.go
+++ main.go
@@ -15,56 +15,65 @@ import (
 	"time"
 )
 
-const Chunk = 65536
 const Slack = 28
+const Chunk = 65536
+const Magic = "**\x00\x00"
 
 var buf0 = make([]byte, 0, Chunk)
-var buf2 = make([]byte, 2)
 var buf4 = make([]byte, 4)
 var buf8 = make([]byte, 8)
 
 func main() {
-	var t uint64
-	var n uint32
+	var x uint64
+	var y uint32
 	var b []byte
 
-	defer func() {
-		if err := recover(); err != nil {
-			_, _ = fmt.Fprintln(os.Stderr, "xr:", err)
-			os.Exit(1)
-		}
-	}()
+	/*
+		defer func() {
+			if err := recover(); err != nil {
+				_, _ = fmt.Fprintln(os.Stderr, "xr:", err)
+				os.Exit(1)
+			}
+		}()
+	*/
 
 	for r := bufio.NewReaderSize(os.Stdin, Chunk); ReadUntil(r); {
-		if n = ReadUint32(r); n < Slack || n > Chunk {
+		if y = ReadUint32(r); y < Slack || y > Chunk {
+			println("abort: size", y)
 			continue // check sane size
 		}
 
-		if t = ReadUint64(r); t == 0 {
+		if x = ReadUint64(r); x == 0 {
+			println("abort: record id", x)
 			continue // check valid record id
 		}
 
-		if t = ReadUint64(r); t == 0 {
+		if x = ReadUint64(r); x == 0 {
+			println("abort: time", x)
 			continue // check valid time
 		}
 
-		if b = ReadBytes(r, buf0[:n-Slack]); len(b) < 18 {
-			continue // check valid binxml length
+		if b = ReadBytes(r, buf0[:y-Slack]); len(b) < 18 {
+			println("abort: stream", len(b))
+			continue // check valid stream length
 		}
 
-		if n != ReadUint32(r) {
+		if y != ReadUint32(r) {
+			println("abort: copy", y)
 			continue // check size equals copy
 		}
 
-		if n = binary.LittleEndian.Uint32(b[14:]); n > 20 {
-			continue // check substitution array length
+		if y = binary.LittleEndian.Uint32(b[14:]); y > 20 {
+			println("abort: items", y)
+			continue // check substitution items
 		}
 
 		if b[28] != 0x06 || b[29] != 0 {
+			println("abort: event id")
 			continue // check event id type and null
 		}
 
-		fmt.Printf("XR|%s|%d\n", FileTime(t).Format("2006-01-02 15:04:05.0000000Z"), binary.LittleEndian.Uint16(b[(n*4)+22:]))
+		fmt.Printf("Record Time: %s EventID: %d\n", FileTime(x).Format("2006-01-02 15:04:05.0000000Z"), binary.LittleEndian.Uint16(b[(y*4)+22:]))
 	}
 }
 
@@ -80,10 +89,6 @@ func ReadUint32(r io.Reader) uint32 {
 	return binary.LittleEndian.Uint32(ReadBytes(r, buf4))
 }
 
-func ReadUint16(r io.Reader) uint16 {
-	return binary.LittleEndian.Uint16(ReadBytes(r, buf2))
-}
-
 func ReadBytes(r io.Reader, b []byte) []byte {
 	if n, err := r.Read(b); err == nil {
 		return b[:n]
@@ -92,18 +97,18 @@ func ReadBytes(r io.Reader, b []byte) []byte {
 	}
 }
 
-func ReadUntil(r io.Reader) bool {
-	for string(buf2) != "**" {
-		switch _, err := io.ReadFull(r, buf2); {
-		case errors.Is(err, io.ErrUnexpectedEOF):
-			return false
+func ReadUntil(r *bufio.Reader) bool {
+	for {
+		switch b, err := r.Peek(4); {
+		default:
+			_, _ = r.Discard(1)
+		case string(b) == Magic:
+			_, _ = r.Discard(4)
+			return true
 		case errors.Is(err, io.EOF):
 			return false
 		case err != nil:
 			panic(err)
 		}
 	}
-
-	ReadUint16(r)
-	return true
 }
